d0f6df2380973dd96e7fb4f24dd06c337e1decbe
Jeramie Andrews <drewsj00@gmail.com>
2020-06-03 10:28:50 -0500

    Complete - 1 commit due to wrong path on push

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Counter from './Components/Counter';
+import Display from './Components/Display';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>Counter App</h2>
-      </header>
-      <Counter/>
+    <div className='App'>
+      <header className='App-header'></header>
+
+      <Counter />
     </div>
   );
 }
--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import Display from './Display';
 import ButtonPad from './ButtonPad';
 
-
 function Counter() {
-    return (
-        <div className="counter">
-            <Display/>
-            <ButtonPad/>
-        </div>
-    )
+  return (
+    <div className='counter'>
+      <Display />
+      <ButtonPad />
+    </div>
+  );
 }
 
 export default Counter;
